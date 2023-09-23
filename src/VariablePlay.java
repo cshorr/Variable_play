7ebaaea6f292052ed8a6aadcf430e8f960d4eba8
cshor <cshortt707@gmail.com>
2023-09-22 18:17:21 -0700

    attempt number 1

---
+++ src/VariablePlay.java
@@ -0,0 +1,26 @@
+
+
+public class VariablePlay {
+    /* Part 1: Naming Variables */
+
+    static String iAmVariable = "Hello";
+    static int iAmInteger = 32;
+    static double iAmDouble = Math.PI;
+    static float iAmFloat = 2.5f;
+
+   static boolean iAmBoolean=true;
+
+    /* part 2 changing a value */
+    public static void main(String []args){
+    boolean iAmBoolean = false;
+        System.out.println(iAmBoolean);
+        System.out.println(VariablePlay.iAmBoolean);
+
+
+    }
+    }
+
+
+
+
+
